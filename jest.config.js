e379d8a551fc9437f84f781efdcab3b119646536
Apurva Ojas <apurvaojas10@gmail.com>
2020-11-07 23:36:17 +0530

    build(jest): test coverage check pre commit hook

--- jest.config.js
+++ jest.config.js
@@ -18,5 +18,13 @@ module.exports = {
     'ts-jest': {
       babelConfig: true,
     }
+  },
+  "coverageThreshold": {
+    "global": {
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": 80
+    }
   }
 };
--- nyc.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  all: true,
-  include: ["packages/**/src/**/*.ts"],
-  exclude: ["**/*.spec.js"],
-  branches: 80,
-  lines: 80,
-  functions: 80,
-  statements: 80,
-};
