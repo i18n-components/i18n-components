5a832ceda723660ba47b150bc6eb82e52b58a08e
Apurva Ojas <apurvaojas10@gmail.com>
2020-11-15 12:48:35 +0530

    docs(setup): deleting generated files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\apuojas\\Projects\\i18n-components\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\apuojas\\Projects\\i18n-components\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\apuojas\\Projects\\i18n-components\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\apuojas\\Projects\\i18n-components\\node_modules\\bootstrap\\dist\\css\\bootstrap.min.css"),
-];
